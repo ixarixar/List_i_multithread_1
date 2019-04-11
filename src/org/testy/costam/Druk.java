48b839d72e362ced24d925761844d0ca3f97a448
ixarixar <ixar@o2.pl>
2019-04-11 12:16:44 +0200

    Final, ok.

--- src/org/testy/costam/Druk.java
+++ src/org/testy/costam/Druk.java
@@ -1,18 +1,19 @@
 package org.testy.costam;
 
+import java.io.IOException;
+import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class Druk {
 	
 	private static Druk INSTANCE;
-	
-	private Object lock = new Object();
-	private List<Integer> kolejka = new ArrayList<Integer>();
+		
+	private List<Drukarka> drukarki;
 	
 	private Druk(){
+		drukarki = new ArrayList<Drukarka>();
     }
-
 	
     public static Druk getInstance() {
         if (INSTANCE == null)
@@ -23,33 +24,88 @@ public final class Druk {
         return INSTANCE;
     }
     
-    
-    public void add(int value){
-    	kolejka.add(value);
-    	System.out.println("        +" + value);
+    public class  Drukarka{
+    	private String hostName;
+    	private String printerName;
     	
-    	Thread t = new Thread(new Runnable() {
-    	    public void run() {
-    	    	
-    	    	synchronized(lock){
-    	    		System.out.println(value + " . . .");
-        	    	try {
-    					//wait();
-    					Thread.sleep(5+value*20);
-    				} catch (InterruptedException e) {
-    					e.printStackTrace();
-    				}
-        	    	System.out.println("           . . . " + value);
-    	    	}
-    	    	
-    	    }
-    	});
-
-    	t.start();
+    	public Drukarka(String printerName, String hostName){
+    		this.printerName = printerName;
+    		this.hostName = hostName;
+    	}
     	
+    	public void setPrinterName(String printerName){
+    		this.printerName = printerName;
+    	}
+    	public String getPrinterName(){
+    		return this.printerName;
+    	}
+    	public void setHostName(String hostName){
+    		this.hostName = hostName;
+    	}
+    	public String getHostName(){
+    		return this.hostName;
+    	}
     }
+    
+    
+    
+	public void addDrukarka(String printerName, String hostName) {
+    	//System.out.println("addDrukarka: printerName: '" + printerName + "' hostName: '" + hostName + "'");
+    	drukarki.add(new Drukarka(printerName, hostName));
+	}
 
-
+    
+    
+    /**
+     * 
+     * @param printerName - nazwa drukarki
+     * @param data - dane do wydruku
+     */
+	public void addZadanie(String printerName, String data) {
+		
+		//szukamy drukarki w dodanych
+		drukarki.forEach( el->{
+			if(el.getPrinterName().equals(printerName)){
+				
+				//wydruk w nowym w¹tku 
+				new Thread(()->{
+					//System.out.println("        +" + "'" + nazwaDrukarki + "':" + w);
+					
+					//synchronizujemy wiele jednoczesnych wydruków - ¿eby siê nie nachodzi³y
+					synchronized(el){
+												
+	        	    	try {
+	    					//wait();
+	    					Thread.sleep(100);
+	    					
+	    					LPR lpr = new LPR();	    					
+	    					
+	    					String printDada = "";
+	    					printDada = "N\n";//Clear Image Buffer
+	    					printDada+= "I8,B,048\n";//Character set selection - 8 bit data, B - Windows1250, 048 - country code 
+	    					//ptext+= "B10,50,0,2C,2,5,50,B,\""+ids+"\"\n";
+	    					printDada+=data;
+	    					printDada+="P1\n";//Print
+	    								
+	    					System.out.print(el.getPrinterName() + ": " +printDada);
+	    					
+	    					lpr.printString(printDada, "", el.getHostName(), el.getPrinterName(), "");
+	    					
+	    					Thread.sleep(100);
+	    					
+	        	    	}catch(ConnectException e){
+	        				e.printStackTrace();
+	        			}catch(IOException e){
+	        				e.printStackTrace();
+	        			} catch (InterruptedException e) {
+	    					e.printStackTrace();
+	    				}
+	    	    	}//synch
+					
+				}).start();
+			}
+		});
+	}
 	
 	
 }
--- src/org/testy/costam/Main.java
+++ src/org/testy/costam/Main.java
@@ -1,18 +1,7 @@
 package org.testy.costam;
 
-public class Main extends Thread{
+public class Main {
 	
-	private int val;
-	private Main(int val){
-		this.val = val;
-	}
-	
-	
-	public void run(){
-        System.out.println("T " + this.val);
-        Druk druk = Druk.getInstance();
-        druk.add(this.val);
-	}
 	
 	
 	public static void main(String[] args) throws InterruptedException {
@@ -21,98 +10,20 @@ public class Main extends Thread{
 		
 		Druk druk = Druk.getInstance();
 		
-		for(int i=0; i<15; i++){
-			druk.add(i);
-			Thread.sleep(40);
-		}
+		//rejestracja
+		druk.addDrukarka("Drukarka-1", "172.16.26.20");
+		
+		//ksiêgowoœæ
+		druk.addDrukarka("Drukarka2_dr-2", "172.16.26.21");
 		
 		
-		/*
-		
-		for(int i=0; i<5; i++){
-			(new Main(i)).start();
-		}
-		Thread.sleep(300);
-		
-		for(int i=5; i<10; i++){
-			(new Main(i)).start();
-		}
-		Thread.sleep(300);
-		
-		for(int i=10; i<15; i++){
-			(new Main(i)).start();
-		}
-		
-		Thread.sleep(300);
-		
-		for(int i=15; i<20; i++){
-			(new Main(i)).start();
-		}
-		
-		
-		*/
-		
-		
-        
-		/*
-		Main m = new Main();
-		m.new Wyslij().start();
-		*/
-		
-        /*
-		
-		for(int i=1; i<10; i++){
-			
-			
-			Thread thread = new Thread() {
-			    public void run() {
-			    	try {
-						Thread.sleep((int)(Math.random ()*500)+10);
-						Druk.add(i);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-			    	
-			    }
-			};
+		druk.addZadanie("Drukarka-1", "A10,0,0,1,4,4,N,\" test1\"\nB10,50,0,2C,2,5,50,B,\"71830\"\n");
 
-			thread.start();
-			
-			
-			
-			new Thread(() -> {
-				try {
-					Thread.sleep((int)(Math.random ()*500)+10);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				Druk.add(i);
-			}).start();
-			
-			
-			try {
-				Thread.sleep((int)(Math.random ()*500)+10);
-			} catch(InterruptedException e){
-				System.out.println("sleep  przerwane!");
-			}
-			
-			Druk.add(i);
-			
-		}
-		
-		
-		*/
 		
 		
 		
-		/*
-		Bufor bufor = new BuforMulti();
-		for(int i = 1; i < 10; i++) {
-			(new  Producent(bufor , "P" + i)). start ();
-			(new  Konsument(bufor , "K" + i)). start ();
-		}
-		*/
+		
+		
 		
 	}
 	
